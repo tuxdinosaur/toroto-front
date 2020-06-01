6d31147acbf91dcd829d378b7b7789f52231d92c
tuxdinosaur <ivan.tux@outlook.com>
2020-06-01 05:13:03 -0500

    :sparkles: [ADD] Se agregó gráfica de prueba

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HomeDescription from './components/HomeDescription'
 import LogForm from './components/LogForm'
+
+import { Line } from 'react-chartjs-2'
 // import logo from './logo.svg';
 
 // import './assets/styles/App.css';
@@ -11,6 +13,33 @@ import LogForm from './components/LogForm'
 // import Home from './views/Home'
 
 function App () {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  }
+
   return (
     <div className='App'>
 
@@ -33,6 +62,15 @@ function App () {
 
       </div>
 
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <Line
+              data={data}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
